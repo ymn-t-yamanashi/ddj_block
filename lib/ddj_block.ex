5f7b41d7b13d5e003f76cfda6778cc893a3f56da
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-12-11 20:15:10 +0900

    画面の追加

--- lib/ddj_block.ex
+++ lib/ddj_block.ex
@@ -2,17 +2,27 @@ defmodule DdjBlock do
   @moduledoc """
   Documentation for `DdjBlock`.
   """
+  alias Rayex.Structs.Rectangle
+  use Rayex
 
-  @doc """
-  Hello world.
+  def main() do
+    init_window(800, 800, "DdjBlock")
+    main_loop(true)
+  end
 
-  ## Examples
+  defp main_loop(true) do
+    begin_drawing()
+    draw()
+    end_drawing()
 
-      iex> DdjBlock.hello()
-      :world
+    Process.sleep(100)
+    main_loop(!window_should_close())
+  end
 
-  """
-  def hello do
-    :world
+  defp main_loop(_), do: nil
+
+  defp draw() do
+    rectangle = %Rectangle{x: 100.0, y: 100.0, width: 100.0, height: 50.0}
+    draw_rectangle_lines_ex(rectangle, 1, %{r: 0, g: 255, b: 0, a: 255})
   end
 end
--- test/ddj_block_test.exs
+++ test/ddj_block_test.exs
@@ -1,8 +1,3 @@
 defmodule DdjBlockTest do
   use ExUnit.Case
-  doctest DdjBlock
-
-  test "greets the world" do
-    assert DdjBlock.hello() == :world
-  end
 end
