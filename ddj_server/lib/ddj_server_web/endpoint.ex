ce34ddc7834bc83c37d1c3b9be33906595af8926
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-12-12 00:25:00 +0900

    mix phx.gen.socket Ddj

--- ddj_server/lib/ddj_server_web/endpoint.ex
+++ ddj_server/lib/ddj_server_web/endpoint.ex
@@ -15,6 +15,12 @@ defmodule DdjServerWeb.Endpoint do
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
 
+
+  socket("/socket", DdjServerWeb.DdjSocket,
+    websocket: true,
+    longpoll: false
+  )
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
