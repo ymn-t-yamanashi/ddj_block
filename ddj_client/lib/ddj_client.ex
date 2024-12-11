006f2622c8a571d181d7e3544cf7fa4b7c42b27b
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-12-12 02:41:08 +0900

    receive修正

--- ddj_client/lib/ddj_client.ex
+++ ddj_client/lib/ddj_client.ex
@@ -6,38 +6,52 @@ defmodule DdjClient do
   alias Socket.Web
 
   def hello do
-
-    socket = Web.connect!("localhost", 4000, path: "/socket/websocket?token=undefined&vsn=2.0.0")
-    |> join()
+    socket =
+      Web.connect!("localhost", 4000, path: "/socket/websocket?token=undefined&vsn=2.0.0")
+      |> join()
 
     {_, input} = PortMidi.open(:input, "DDJ-FLX4 MIDI 1")
     PortMidi.listen(input, self())
     midi_in(input, socket)
     PortMidi.close(:input, input)
 
-
     :world
-
   end
 
   def midi_in(input, socket) do
     receive do
-      {^input, [{{176, 33, 65}, _}]} ->
-        send_data(socket, "右")
-        midi_in(input, socket)
-      {^input, [{{176, 33, 63}, _}]} ->
-        send_data(socket, "左")
-        midi_in(input, socket)
-      _ ->
-        midi_in(input, socket)
+      a ->
+        mi(a, socket)
+        # {^input, [{{176, 33, 65}, _}]} ->
+        #   # send_data(socket, "右")
+        #   IO.inspect("d")
+        # {^input, [{{176, 33, 63}, _}]} ->
+        #   # send_data(socket, "左")
+        #   IO.inspect("c")
     end
+
+    midi_in(input, socket)
   end
 
+  def mi({_, [{{176, 33, 65}, _}]}, socket) do
+    IO.inspect("右")
+    send_data(socket, "右")
+  end
+
+  def mi({_, [{{176, 33, 63}, _}]}, socket) do
+    IO.inspect("左")
+    send_data(socket, "左")
+  end
+
+  def mi(_, _) do
+    nil
+  end
 
   def send_data(socket, data) do
     data = """
     ["3","4","ddj:lobby","new_msg",{"body":"#{data}"}]
     """
+
     Web.send!(socket, {:text, data})
     # Process.sleep(250)
     socket
@@ -47,6 +61,7 @@ defmodule DdjClient do
     phx_join = """
     ["3","3","ddj:lobby","phx_join",{}]
     """
+
     Web.send!(socket, {:text, phx_join})
     socket
   end
