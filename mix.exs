ebcc749753630b393ed4ea22f42fb1d1d6d46937
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-12-11 18:40:30 +0900

    rayexとportmidi追加

--- mix.exs
+++ mix.exs
@@ -14,13 +14,15 @@ defmodule DdjBlock.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :portmidi]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:rayex, "~> 0.0.3"},
+      {:portmidi, "~> 5.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
