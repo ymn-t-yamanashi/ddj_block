17371c76a0b03f3db5f069eb5fd49225df02a234
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-12-12 00:35:32 +0900

    mix new ddj_client

---
+++ ddj_client/lib/ddj_client.ex
@@ -0,0 +1,18 @@
+defmodule DdjClient do
+  @moduledoc """
+  Documentation for `DdjClient`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DdjClient.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
