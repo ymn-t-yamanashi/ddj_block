8cf263ed91920a10f4aca30a39a444b8587dcc1f
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-12-12 00:43:55 +0900

    DdjClient作成

--- ddj_client/lib/ddj_client.ex
+++ ddj_client/lib/ddj_client.ex
@@ -3,16 +3,31 @@ defmodule DdjClient do
   Documentation for `DdjClient`.
   """
 
-  @doc """
-  Hello world.
+  alias Socket.Web
 
-  ## Examples
-
-      iex> DdjClient.hello()
-      :world
-
-  """
   def hello do
+    Web.connect!("localhost", 4000, path: "/socket/websocket?token=undefined&vsn=2.0.0")
+    |> join()
+    |> send_data("data")
+
     :world
+
+  end
+
+  def send_data(socket, data) do
+    data = """
+    ["3","4","ddj:lobby","new_msg",{"body":"#{data}"}]
+    """
+    Web.send!(socket, {:text, data})
+    Process.sleep(250)
+    socket
+  end
+
+  def join(socket) do
+    phx_join = """
+    ["3","3","ddj:lobby","phx_join",{}]
+    """
+    Web.send!(socket, {:text, phx_join})
+    socket
   end
 end
