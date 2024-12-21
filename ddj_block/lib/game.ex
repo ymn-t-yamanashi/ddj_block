c83abfd564261fb23d649ca748581544c11565ce
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-12-21 11:24:01 +0900

    あたり判定作成

---
+++ ddj_block/lib/game.ex
@@ -0,0 +1,23 @@
+defmodule Game do
+  @doc """
+  Collided detection.
+
+  ## Examples
+
+      iex> Game.collided?(1, 1, 10, 10, 5, 5, 5, 5)
+      true
+
+  """
+  def collided?(x1, y1, w1, h1, x2, y2, w2, h2) do
+    x1 <= x2 + w2 && x1 + w1 >= x2 && y1 <= y2 + h2 && y1 + h1 >= y2
+  end
+
+  def collided?(x1, y1, w1, h1, judgments) do
+    judgments
+    |> Enum.map(fn judgement ->
+      [x2, y2, w2, h2] = judgement
+      collided?(x1, y1, w1, h1, x2, y2, w2, h2)
+    end)
+    |> Enum.any?()
+  end
+end
